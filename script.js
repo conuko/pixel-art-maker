8e01e96f3f180c32a311b925260a7d50dad6c69b
Constantin Unterkofler <constantinu93@gmail.com>
2020-11-23 22:47:58 +0100

    function to give color to clicked cells

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ function form() {
     // get height for grid
     const gridHeight = document.getElementById('inputHeight').value;
 }
+
+// Event listener of each of grid's cell to make it possible to give color to the clicked cells
+function eventForCells() {
+    // get all cells
+    const numberOfCells = document.getElementsByClassName('cell');
+    // get color picker element
+    const colorPicker = document.getElementById("colorPicker");
+    // loop over all cells
+    for (let i = 0; i < numberOfCells.length; i++) {
+        // give the clicked cell the with "colorPicker" choosen color
+        numberOfCells[i].addEventListener("click", (event) => {
+            let cellClick = event.target;
+            cellClick.style.backgroundColor = colorPicker.value;
+        });
+    }
+}
